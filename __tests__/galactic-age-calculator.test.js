affbb7897a899e2051391667b712e8ded3bec159
George Acosta <georgeacostar@gmail.com>
2019-11-15 13:49:22 -0800

    test 5 setActLev func meaningfull fail

--- __tests__/galactic-age-calculator.test.js
+++ __tests__/galactic-age-calculator.test.js
@@ -1,20 +1,25 @@
 import { Person } from "./../src/galactic-age-calculator.js";
 
 test('should correctly convert 5 earth years to (20.83) mercury years', () => {
-    let testMercPerson = new Person(5);
-    expect(testMercPerson.calcMercYears()).toEqual("20.83");
-  });
+  let testMercPerson = new Person(5);
+  expect(testMercPerson.calcMercYears()).toEqual("20.83");
+});
 
-  test('should correctly convert 5 earth years to (8.06) venus years', () => {
-    let testVenPerson = new Person(5);
-    expect(testVenPerson.calcVenYears()).toEqual("8.06");
-  }); 
+test('should correctly convert 5 earth years to (8.06) venus years', () => {
+  let testVenPerson = new Person(5);
+  expect(testVenPerson.calcVenYears()).toEqual("8.06");
+}); 
 
-  test('should correctly convert 5 earth years to (2.66) Mars years', () => {
-    let testMarsPerson = new Person(5);
-    expect(testMarsPerson.calcMarsYears()).toEqual("2.66");
-  });
-  test.only('should correctly convert 5 earth years to (0.42) Jupiter years', () => {
-    let testJupPerson = new Person(5);
-    expect(testJupPerson.calcJupYears()).toEqual("0.42");
-  });
+test('should correctly convert 5 earth years to (2.66) Mars years', () => {
+  let testMarsPerson = new Person(5);
+  expect(testMarsPerson.calcMarsYears()).toEqual("2.66");
+});
+test('should correctly convert 5 earth years to (0.42) Jupiter years', () => {
+  let testJupPerson = new Person(5);
+  expect(testJupPerson.calcJupYears()).toEqual("0.42");
+});
+test.only('should correctly set the activity level for person const', () => {
+  let testSetActLev = new Person();
+  testSetActLev.setActivityLevel("Not Active")
+  expect(testSetActLev.activityLevel).toEqual("Very Active");
+});
--- src/galactic-age-calculator.js
+++ src/galactic-age-calculator.js
@@ -1,27 +1,47 @@
 export class Person {
 
-constructor(age) {
-    this.age = age 
+  constructor(age, activityLevel, lifeExp) {
+    this.age = age, 
+    this.activityLevel = activityLevel,
+    this.lifeExp = 0
   }
 
-calcMercYears(age) {
+  calcMercYears(age) {
     const mercYears = (this.age * 365) / (365 * 0.24);
     return mercYears.toFixed(2);   
-}
+  }
 
-calcVenYears(age) {
+  calcVenYears(age) {
     const venYears = (this.age * 365) / (365 * 0.62);
     return venYears.toFixed(2);   
-}
+  }
 
-calcMarsYears(age) {
+  calcMarsYears(age) {
     const marsYears = (this.age * 365) / (365 * 1.88);
     return marsYears.toFixed(2);   
-}
+  }
 
-calcJupYears(age) {
+  calcJupYears(age) {
     const jupYears = (this.age * 365) / (365 * 11.86);
     return jupYears.toFixed(2);   
-}
+  }
+
+  setActivityLevel(level){ 
+    if ( level === "Not Active"){
+      this.activityLevel = "Not Active"
+    }
+    if ( level === "Very Active"){
+      this.activityLevel = "Very Active" 
+    }
+  }
+
+//   calcLifeExp(activitylevel) {
+//     if (activitylevel === "Not Active"){
+//       return this.lifeExp += 5;
+//     } 
+//     if (activitylevel === "Very Active") {
+//       return this.lifeExp += 15;
+//     }
+//   }
 
 }
