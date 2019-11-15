b5f8b9bdd98d4f116731e10412aad7eb32878819
George Acosta <georgeacostar@gmail.com>
2019-11-15 12:40:51 -0800

    fix calc.js term

--- src/galactic-age-calculator.js
+++ src/galactic-age-calculator.js
@@ -5,22 +5,22 @@ constructor(age) {
   }
 
 calcMercYears(age) {
-    let mercYears = (this.age * 365) / (365 * 0.24);
+    const mercYears = (this.age * 365) / (365 * 0.24);
     return mercYears.toFixed(2);   
 }
 
 calcVenYears(age) {
-    let venYears = (this.age * 365) / (365 * 0.62);
+    const venYears = (this.age * 365) / (365 * 0.62);
     return venYears.toFixed(2);   
 }
 
 calcMarsYears(age) {
-    let marsYears = (this.age * 365) / (365 * 1.88);
+    const marsYears = (this.age * 365) / (365 * 1.88);
     return marsYears.toFixed(2);   
 }
 
 calcJupYears(age) {
-    let jupYears = (this.age * 365) / (365 * 11.86);
+    const jupYears = (this.age * 365) / (365 * 11.86);
     return jupYears.toFixed(2);   
 }
 
